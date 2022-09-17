deaff2b57eceb1cd821c87181e3805637dd99ee9
Mohamed0400 <mohamedmegahed298.mm@gmail.com>
2022-09-17 21:11:56 +0200

    First commit to the  newsleeter app

--- app.js
+++ app.js
@@ -61,8 +61,8 @@ app.post("/failure" , (req , res) => {
 });
 
 
-app.listen( process.env.PORT || 3001 , () => {
-  console.log("Server is running on port 3001");
+app.listen( process.env.PORT || 3000 , () => {
+  console.log("Server is running on port 3000");
 });
 
 
